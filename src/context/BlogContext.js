1e873a92bc17eccf2b6685141763250018078a97
Robert L. Kushner IV <robertkushner4@gmail.com>
2020-08-07 13:20:31 -0400

    context and provider added

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
+    const [blogPosts, setBlogPosts] = useState([]);
+
+    const addBlogPost = () => {
+        setBlogPosts([...blogPosts, { title: `Blog Post #${blogPost.length + 1}` }]);
+    };
+
     return (
-        <BlogContext.Provider>
+        <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
             {children}
         </BlogContext.Provider>
     )
 };
+
+export default BlogContext;
