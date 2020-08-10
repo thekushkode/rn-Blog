1563682a2306c601b4d519959bf8862d3fc6471d
Robert L. Kushner IV <robertkushner4@gmail.com>
2020-08-10 16:07:03 -0400

    edit screen save

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,10 +1,27 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useContext } from 'react';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
+import { Context } from '../context/BlogContext';
 
 const EditScreen = ({ navigation }) => {
+
+    const { state } = useContext(Context);
+    const blogPost = state.find((blogPost) => blogPost.id === navigation.getParam('id'));
+    const [title, setTitle] = useState(blogPost.title);
+    const [content, setContent] = useState(blogPost.content);
+
     return (
         <View>
-            <Text>Edit Screen - {navigation.getParam('id')}</Text>
+            <View>
+            <Text style={styles.label}>Edit Title:</Text>
+            <TextInput style={styles.input} value={title} onChangeText={(newTitle) => setTitle(newTitle)} />
+            <Text style={styles.label}>Edit Content:</Text>
+            <TextInput style={styles.input} value={content} onChangeText={(newContent) => setContent(newContent)} />
+            <Button title="Add Blog Post" onPress={() => {
+                addBlogPost(title, content, () => {
+                    navigation.navigate('Index');
+                });
+            }} />
+        </View>
         </View>
     )
 };
